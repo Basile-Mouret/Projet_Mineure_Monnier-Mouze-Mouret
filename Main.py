ccab208683961e98ba58caa6789b5b427463c205
Basile Mouret <145753507+Basile-Mouret@users.noreply.github.com>
2023-10-04 10:21:34 +0200

    GUille+BAZ3

--- Main.py
+++ Main.py
@@ -73,6 +73,52 @@ class graphe :
         file = open("Latex/latex.tex", "w")
         file.write(tex)
         file.close()
+        
+    
+    def contient_cycle(self):
+        """
+        Détecte la présence d'éventuels cycles dans le graphe
+        :return: un booléen (vrai si le graphe contient un cycle, faux sinon) 
+        """
+        T=[]
+        for noeud1 in T:
+            for noeud2 in T:
+                if arc_present(self,noeud1,noeud2):
+                    T.append([noeud1,noeud2])
+        for b in range(10):
+            for i in range(len(T)):
+                for j in range(len(T)):
+                    if T[i][-1]==T[j][0]:
+                        T[i].append(T[j][-1])
+                        T[j]=0
+        return T
+
+                   
+    
+def arc_present(g, noeud1, noeud2):
+  """
+  Indique s'il existe un arc entre deux points
+  :param neoud1 et noeud2: noeuds entre lesquels on veut savoir s'il existe un arc
+  :return: un booléen (vrai s'il existe un arc entre les deux points, faux sinon)
+  """
+  depart = noeud1
+  arrivee = noeud2
+  file = []
+  visites = {element: False for element in g.noeuds}
+  file.append(depart)
+
+  while file:
+    actuel = file.pop(0)
+    visites[actuel] = True
+    for noeud in g.noeuds:
+      if noeud in g.adj[actuel] and visites[noeud] == False:
+        file.append(noeud)
+        visites[noeud] = True
+        if noeud == arrivee:
+          return True
+  return False
+
+
             
 def main():
     grCours = graphe(set(range (10)),{(5,8),(8,2),(2,9),(4,8),(4,0),(0,7),(7,6),(2,4),(8,1),(1,3),(1,6)})
